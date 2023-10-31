86d1d6c7dcb022500ec78c8fccbe2fe84dd366e4
yu <yu@sec.com>
2023-10-31 16:24:28 +0800

    添加一个mod

---
+++ ChangeMapMod/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeMapMod
+{
+    public class Class1
+    {
+        /**
+         * 
+         * 打算做一个修改地图大小的mod.
+         * 并不删除垃圾.
+         * 
+         * 1. 修改地图大小
+         * 2. 修改流星
+         * 3. 修改技能点和存档
+         * 4. 修改小行星位置
+         * 5. 
+         * 
+         * 
+         * 
+         */
+    }
+}
