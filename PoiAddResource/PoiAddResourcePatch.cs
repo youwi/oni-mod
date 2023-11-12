c5277bf8c56d77fb6e30d27f219c400080edc249
yu <yu@sec.com>
2023-11-12 20:13:21 +0800

    修改图片

--- PoiAddResource/PoiAddResourcePatch.cs
+++ PoiAddResource/PoiAddResourcePatch.cs
@@ -1,36 +1,20 @@
 ﻿using HarmonyLib;
+using System;
+ 
 using System.Collections.Generic;
+ 
+using static HarvestablePOIConfig;
+using static HarvestablePOIConfigurator;
 
 namespace PoiAddResource
 {
-    // HarvestablePOIConfig.GenerateConfigs()
 
-
-    [HarmonyPatch(typeof(HarvestablePOIConfig), "GenerateConfigs")]
-    public static class PoiAddResourcePatch
+   // [HarmonyPatch(typeof(HarvestablePOIConfig), "GenerateConfigs", new Type[] { })]
+    public class PoiAddResourcePatch
     {
-        public static void Postfix(List<HarvestablePOIConfig.HarvestablePOIParams> __result)
+        public static List<HarvestablePOIParams> patchList(List<HarvestablePOIParams> list)
         {
-            //List<HarvestablePOIConfig.HarvestablePOIParams> list = new List<HarvestablePOIConfig.HarvestablePOIParams>();
-
-            //list.Add(new HarvestablePOIConfig.HarvestablePOIParams(
-            //    "satellite_field",
-            //    new HarvestablePOIConfigurator.HarvestablePOIType(
-            //        "SatelliteField",
-            //        new Dictionary<SimHashes, float>{
-            //            { SimHashes.Sand,3f},
-            //            { SimHashes.IronOre,3f },
-            //            { SimHashes.MoltenCopper,2.67f },
-            //            { SimHashes.Glass, 1.33f}
-            //        },
-            //        30000f,
-            //        45000f,
-            //        30000f,
-            //        60000f,
-            //        true,
-            //        HarvestablePOIConfig.AsteroidFieldOrbit, 20, "EXPANSION1_ID")));
-
-            foreach(var tmp in __result)
+            foreach (var tmp in list)
             {
                 if (tmp.anim == "radioactive_gas_cloud")
                 {
@@ -40,9 +24,97 @@ namespace PoiAddResource
                     tmp.poiType.harvestableElements.Add(SimHashes.Radium, 0.5f);//镭
                     tmp.poiType.harvestableElements.Add(SimHashes.SolidMercury, 0.5f);//汞固体
                     tmp.poiType.harvestableElements.Add(SimHashes.SolidNaphtha, 0.5f);//石脑油 固体
+
+                    tmp.poiType.harvestableElements.Add(SimHashes.SolidResin, 0.1f);//树脂 固体
+                    tmp.poiType.harvestableElements.Add(SimHashes.GoldAmalgam, 1f);//金汞矿 固体
+                    tmp.poiType.harvestableElements.Add(SimHashes.Niobium, 0.1f);//铌 固体
+
+                    break;
                 }
+                //List<HarvestablePOIConfig.HarvestablePOIParams> list = new List<HarvestablePOIConfig.HarvestablePOIParams>();
+
+                //list.Add(new HarvestablePOIConfig.HarvestablePOIParams(
+                //    "satellite_field",
+                //    new HarvestablePOIConfigurator.HarvestablePOIType(
+                //        "SatelliteField",
+                //        new Dictionary<SimHashes, float>{
+                //            { SimHashes.Sand,3f},
+                //            { SimHashes.IronOre,3f },
+                //            { SimHashes.MoltenCopper,2.67f },
+                //            { SimHashes.Glass, 1.33f}
+                //        },
+                //        30000f,
+                //        45000f,
+                //        30000f,
+                //        60000f,
+                //        true,
+                //        HarvestablePOIConfig.AsteroidFieldOrbit, 20, "EXPANSION1_ID")));
+
             }
+            return list;
+        }
+        //public static List<HarvestablePOIParams> Postfix(HarvestablePOIConfig __instance)
+        //{
+        //    List<HarvestablePOIParams> list = __instance;
+        //    list = patchList(list);
+        //    return list;
+        //}
+        public static void Postfix(HarvestablePOIConfig __instance, ref  List<HarvestablePOIParams> __result)
+        {
+
+            // IEnumerable
+            // HarvestablePOIConfig.GenerateConfigs()
+            if (__result == null)
+            {
+                return  ;
+            }
+            Debug.LogWarning("HarvestablePOIConfig.GenerateConfigs()---->");
+            patchList(__result);
+        }
+    }
+
+    [HarmonyPatch(typeof(HarvestablePOIInstanceConfiguration), "GetElementsWithWeights")]
+    public class HarvestablePOIInstanceConfigurationConPatch
+    {
+        static bool inited=false;
+        public static void Postfix(HarvestablePOIConfigurator __instance)
+        {
+            if (!inited)
+            {
+                //HarvestablePOIConfigurator._poiTypes;
+                var list = Traverse.CreateWithType("HarvestablePOIConfigurator")
+                  .Field("_poiTypes")
+                  .GetValue<List<HarvestablePOIType>>();
+                if (list == null)
+                    return;
+            
+                foreach (var tt in list)
+                {
+                
+                        if (tt.id == "RadioactiveGasCloud")
+                        {
+                            tt.harvestableElements.Add(SimHashes.Katairite, 1f);//深渊晶石
+                            tt.harvestableElements.Add(SimHashes.Fossil, 0.5f);//化石
+                            tt.harvestableElements.Add(SimHashes.Radium, 0.5f);//镭
+                            tt.harvestableElements.Add(SimHashes.GoldAmalgam, 1f);//金汞矿 固体
+                            tt.harvestableElements.Add(SimHashes.Niobium, 0.1f);//铌 固体
+                            tt.harvestableElements.Add(SimHashes.Resin, 0.1f);//树脂  solid
+                        //  Debug.LogWarning("HarvestablePOIInstanceConfiguration O00000");
+                            inited = true;
+                        }
+                        if(tt.id == "OilyAsteroidField")
+                        {
+                            tt.harvestableElements.Add(SimHashes.Mercury, 0.5f);//汞   solid
+                            tt.harvestableElements.Add(SimHashes.Naphtha, 0.5f);//石脑油 固体
+                        }
+                    //Debug.LogWarning("HarvestablePOIInstanceConfiguration 11111000000"+ tt.id);
+                  }
+                
+            } 
+           // Debug.LogWarning("HarvestablePOIInstanceConfiguration ---->");
 
         }
     }
+
+
 }
