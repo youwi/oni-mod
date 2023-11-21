4d45819555594d267189e8e4f5aa4eb46e33619c
yu <yu@sec.com>
2023-11-21 11:12:02 +0800

    添加新mod

---
+++ LimitHydroponicMod/ConduitConsumerPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimitHydroponicMod
+{
+
+    [HarmonyPatch(typeof(ConduitConsumer), "Consume")]
+    public class ConduitConsumerPatch
+    {
+
+        public  void Prefix()
+        {
+            //  ConduitConsumer conduitConsumer = go.AddOrGet<ConduitConsumer>();
+           // conduitConsumer.alwaysConsume = false;
+           
+        }
+    }
+}
