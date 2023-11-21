def029e85bc8e128eb256cefe02a4feeeb06f4ba
yu <yu@sec.com>
2023-11-21 13:44:50 +0800

    改了液体

--- LimitHydroponicMod/ConduitConsumerPatch.cs
+++ LimitHydroponicMod/ConduitConsumerPatch.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using STRINGS;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,12 +25,14 @@ namespace LimitHydroponicMod
                 //__instance.storage.ToString();
                 //__instance.gameObject.i
                 // __instance.storage.
-              
+
                 // Debug.LogWarning($"------>.storage A -----{__instance.name} {__instance.storage} {__instance.storage.name} ");
-               // Debug.LogWarning($"------>.storage mass -----{__instance.storage.capacityKg} {__instance.storage.MassStored()}"); 
-               
+                // Debug.LogWarning($"------>.storage mass -----{__instance.storage.capacityKg} {__instance.storage.MassStored()}"); 
+               // __instance.storage.MassStored;
+
+                //__instance.storage.items.
                 //__instance.storage;//如果存量还很多.
-                if (__instance.storage.MassStored() > 1)
+                if (calcLiquidMass(__instance) > 1) //固体,液体都要计算.
                 {
                    // Debug.LogWarning("------>修改了kg.B  -----"+__instance.name);
                     return false;
@@ -42,6 +45,28 @@ namespace LimitHydroponicMod
             }
         
             return true;
+        }
+        public static float calcLiquidMass( ConduitConsumer __instance)
+        {
+            //__instance.storage.MassStored()
+            var items = __instance.storage.items;
+            float num = 0f;
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (!(items[i] == null))
+                {
+                    PrimaryElement component = items[i].GetComponent<PrimaryElement>();
+
+                    if (component.HasTag(GameTags.Liquid)||
+                        component.ElementID == SimHashes.Water
+                      || component.ElementID == SimHashes.DirtyWater
+                      || component.ElementID == SimHashes.SaltWater)
+                    {
+                        num += component.Units;
+                    };
+                }
+            }
+            return (float)Mathf.RoundToInt(num * 1000f) / 1000f;
         }
     }
     //ConfigureBuildingTemplate
