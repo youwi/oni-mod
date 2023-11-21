05fce0801d5b0ec1b69ae8f31e1a8446da70f76a
yu <yu@sec.com>
2023-11-21 12:22:03 +0800

    修改了一部分代码

--- LimitHydroponicMod/ConduitConsumerPatch.cs
+++ LimitHydroponicMod/ConduitConsumerPatch.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace LimitHydroponicMod
 {
@@ -12,11 +13,40 @@ namespace LimitHydroponicMod
     public class ConduitConsumerPatch
     {
 
-        public  void Prefix()
+        public  bool Prefix(ConduitConsumer __instance )
         {
+            //  ConduitUpdate 这个方法上moc
             //  ConduitConsumer conduitConsumer = go.AddOrGet<ConduitConsumer>();
-           // conduitConsumer.alwaysConsume = false;
-           
+            // conduitConsumer.alwaysConsume = false;
+            if (__instance.conduitType ==ConduitType.Liquid
+                && __instance.capacityKG==5f
+                )
+            {
+                //__instance.storage;//如果存量还很多.
+               if(__instance.storage.capacityKg < 2.5)
+                {
+                    Debug.LogWarning("------>修改了kg.  -----"+__instance.name);
+                    return true;
+                }
+                //判定.
+            }
+            else
+            {
+
+            }
+            return false;
         }
     }
+    //ConfigureBuildingTemplate
+
+    [HarmonyPatch(typeof(HydroponicFarmConfig), "ConfigureBuildingTemplate")]
+    public class HydroponicFarmConfigPatch
+    {
+        public void   Postfix(GameObject go)
+        {
+            ConduitConsumer conduitConsumer = go.AddOrGet<ConduitConsumer>();
+            conduitConsumer.consumptionRate = 100f;
+        }
+    }
+
 }
