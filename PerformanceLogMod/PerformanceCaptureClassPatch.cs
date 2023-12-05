e3f2ff0e11a6114791b021c3e07c08f6d7636bf9
yu <yu@sec.com>
2023-12-06 00:41:12 +0800

    限制电力视图更新频率

--- PerformanceLogMod/PerformanceCaptureClassPatch.cs
+++ PerformanceLogMod/PerformanceCaptureClassPatch.cs
@@ -330,6 +330,29 @@ namespace PerformanceLogMod
     //    }
     //}
 
+    //调整刷新速度,看能不能提升性能
+    [HarmonyPatch(typeof(EnergyInfoScreen), "Refresh")]
+    public class EnergyInfoScreen_Refresh_Patch
+    {
+        static float tickSecond = 0;
+        static float tickRefresh = 0;
+        public static bool Prefix()
+        {
+             
+            if (Time.realtimeSinceStartup - tickSecond > 5)
+            {
+                tickSecond = Time.realtimeSinceStartup;
+                return true;
+            }
+            tickRefresh++;
+            if (tickRefresh > 100)
+            {
+                Debug.Log($"---> 刷新次数:{tickRefresh} ");
+                tickRefresh = 0;
+            }
+            return false;
+        }
+    }
     //禁止 腐烂物的消息,可能有助于减少内存. 功能测试中,好像有点用...
 
     [HarmonyPatch(typeof(RotPile), "TryCreateNotification")]
