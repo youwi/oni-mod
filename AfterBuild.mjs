9bde57e174fe4fbb189f99ddcf1fbde9c922a580
yu <yu@sec.com>
2025-09-01 00:59:31 +0800

    修改了日期

--- AfterBuild.mjs
+++ AfterBuild.mjs
@@ -12,7 +12,7 @@ let replaceFileSync=function( filePath, sourceRegx, targetStr){
 function dayVersion() {
     const date = new Date();
     const year = date.getFullYear(); // 年份，例如 2023
-    const month = date.getMonth(); // 月份，0-11，0 表示一月，11 表示十二月
+    const month = date.getMonth()+1; // 月份，0-11，0 表示一月，11 表示十二月
     const day = date.getDate(); // 日期，1-31
     return "version: "+year + "." + month + "." + day
 }
